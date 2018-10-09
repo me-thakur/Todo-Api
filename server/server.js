49a33e9905737eceb8cbd66a62aad1b1ee62bb3b
me-thakur <30404593+me-thakur@users.noreply.github.com>
2018-10-09 14:41:43 +0530

    Test POST /todos route

--- server/server.js
+++ server/server.js
@@ -1,15 +1,19 @@
+//library imports
 var express = require('express');
-var bodyParser = require('body-parser');
+var bodyParser = require('body-parser'); //body parser take the body and pass the JSON
 
+
+//local imports
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
 
-app.use(bodyParser.json());
+app.use(bodyParser.json()); //middleware
 
 app.post('/todos', (req, res) => {
+  //console.log(req.body);
   var todo = new Todo({
     text: req.body.text
   });
@@ -22,5 +26,7 @@ app.post('/todos', (req, res) => {
 });
 
 app.listen(3001, () => {
-  console.log('Started on port 3000');
+  console.log('Started on port 3001');
 });
+
+module.exports = {app};
---
+++ server/tests/server.test.js
@@ -0,0 +1,36 @@
+const expect = require('expect');
+const request = require('supertest');
+
+const {app} = require('./../server');
+const {Todo} = require('./../models/todo');
+
+beforeEach((done) => {
+  Todo.remove({}).then(() => done());
+});
+
+describe('POST/todos', () => {
+  it('should create a new todo', (done) => {
+    var text = 'Test todo text';
+
+    request(app)
+    .post('/todos')
+    .send({text})//sending data with the body as JSON
+    //assertions
+    .expect(200)
+    .expect((res) => {
+      expect(res.body.text).toBe(text);
+    })
+    .end((err, res) => {
+      if(err) {
+        return done(err);
+      }
+
+      //checking data of the database
+      Todo.find().then((todos) => {
+        expect(todos.length).toBe(1);
+        expect(todos[0].text).toBe(text);
+        done();
+      }).catch((e) => done(e));
+    });
+  });
+});

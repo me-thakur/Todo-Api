79dff26f45bfccf607e4fd59a80740951b4116cb
me-thakur <30404593+me-thakur@users.noreply.github.com>
2018-10-14 18:54:45 +0530

    Add post user route and token method

--- server/config/config.js
+++ server/config/config.js
@@ -2,7 +2,7 @@ var env = process.env.NODE_ENV || 'development';
 
 if (env =='development') {
   process.env.PORT = 3001;
-  process.env.MONGO_URI = 'mongodb://localhost:27017/TodoApp', { useNewUrlParser: true };
+  process.env.MONGO_URI = 'mongodb://localhost:27017/TodoApp', { useNewUrlParser: true }
 } else if (env =='test') {
   process.env.PORT = 3001;
   process.env.MONGO_URI = 'mongodb://localhost:27017/TodoAppTest', { useNewUrlParser: true }

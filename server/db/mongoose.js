ec7e169e6b869fdc9f2c6fd8605b1d22da09defa
me-thakur <30404593+me-thakur@users.noreply.github.com>
2018-10-15 15:07:58 +0530

    Add POST/users/login route

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,5 +2,6 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise; //setup to use Promises
 mongoose.connect(process.env.MONGO_URI,{ useNewUrlParser: true}); //connected to mongodb database
+mongoose.set('useCreateIndex', true);
 
 module.exports = {mongoose}
--- server/models/user.js
+++ server/models/user.js
@@ -72,6 +72,26 @@ UserSchema.statics.findByToken = function (token) {
   });
 };
 
+UserSchema.statics.findByCredentials = function (email, password) {
+  var User = this;
+
+  return User.findOne({email}).then((user) => {
+    if(!user) {
+      return Promise.reject();
+    }
+    //bcrypt works on callbacks so a new promise is to be returned
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          resolve(user);
+        }  else {
+          reject();
+        }
+      })
+    });
+  });
+};
+
 //CHANGES BEFROE SAVING TODOS TO THE DATABASE LIKE ADDING A HASHPASSWORD
 UserSchema.pre('save', function (next) {
   var user = this;

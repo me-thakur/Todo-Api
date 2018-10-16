a5cef3a0320f222d8585dfeb6a7f5e1202d6ffc6
me-thakur <30404593+me-thakur@users.noreply.github.com>
2018-10-16 17:02:45 +0530

    Minor changes

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise; //setup to use Promises
-mongoose.connect(process.env.MONGO_URI,{ useNewUrlParser: true}); //connected to mongodb database
+mongoose.connect(process.env.MONGODB_URI); //connected to mongodb database
 mongoose.set('useCreateIndex', true);
 
 module.exports = {mongoose}

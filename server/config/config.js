cf88f12b13087d8fc597c67ed03737c796dc7bcd
me-thakur <30404593+me-thakur@users.noreply.github.com>
2018-10-16 15:54:41 +0530

    Update config to use json file

--- server/config/config.js
+++ server/config/config.js
@@ -1,9 +1,10 @@
 var env = process.env.NODE_ENV || 'development';
 
-if (env =='development') {
-  process.env.PORT = 3001;
-  process.env.MONGO_URI = 'mongodb://localhost:27017/TodoApp', { useNewUrlParser: true }
-} else if (env =='test') {
-  process.env.PORT = 3001;
-  process.env.MONGO_URI = 'mongodb://localhost:27017/TodoAppTest', { useNewUrlParser: true }
+if (env == 'development' || env == 'test') {
+  var config = require('./config.json');
+  var envConfig = config[env];
+
+  Object.keys(envConfig).forEach((key) => {
+    process.env[key] = envConfig[key];
+  });
 }
